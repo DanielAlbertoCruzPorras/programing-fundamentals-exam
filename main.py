51c456b234ad26f17bc98ee61a4c4c33fa3aecd5
DanielAlbertoCruzPorras <83321153+DanielAlbertoCruzPorras@users.noreply.github.com>
2024-11-05 13:15:05 -0500

    Exercise 4

--- main.py
+++ main.py
@@ -1,20 +1,8 @@
-#Exercise 3
-def average(numbers):
-    if len(numbers) == 0:
-        return 0
-    else:
-        return sum(numbers) / len(numbers)
+#Exercise 4
+def toKm():
+    distoKm = float(input("Inserte la distancia en metros: ")) / 1000
 
-def volt():
-    Volt = float(input("Insert the Voltage: "))
-    return Volt
+    return distoKm
 
 # Main
-numbers = [volt(), volt(), volt()]
-
-if average(numbers) >= 220:      #Enunciado no especificaba menor/mayor ó IGUAL, así que así lo dejo...
-    print(f"PELIGRO")
-elif average(numbers) <= 115:
-    print(f"VOLTAJE CORRECTO")
-else:
-    print(f"ALTO VOLTAJE")
+distKm = print(f"Esa distancia equivale a: {toKm():.2f} Km")
