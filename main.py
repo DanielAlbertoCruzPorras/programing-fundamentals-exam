82eb09910f5fa3c6b39da2a5aaaf8dccc054f9fb
DanielAlbertoCruzPorras <83321153+DanielAlbertoCruzPorras@users.noreply.github.com>
2024-11-05 13:14:08 -0500

    Exercise 2

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
-#Exercise 1
-def average(numbers):
-    if len(numbers) == 0:
-        return 0
-    return sum(numbers) / len(numbers)
+#Exercise 2
+import math
 
-def volt():
-    Volt = float(input("Insert the Voltage: "))
-    return Volt
+def area(a):
+    if (math.sqrt(3)/4) * (a ** 2) > 1000:
+        #print(f"{(math.sqrt(3)/4) * (a ** 2):.2f}")  # Para verificar si en efecto el area es mayor a 1000
+        print("DATOS NO VÁLIDOS")
+    else:
+        #print(f"El area de ese triángulo es de: {(math.sqrt(3)/4) * (a ** 2):.2f}")  # No piden imprimir el área pero por si a caso...
+        return ((math.sqrt(3)/4) * (a ** 2))
 
 # Main
-numbers = [volt(), volt(), volt(), volt(), volt()]
-
-if average(numbers) > 220:
-    print(f"ALTO VOLTAJE")
-else:
-    print(f"VOLTAJE CORRECTO")
-
+A = float(input("Inserte la longitud del lado del triángulo: "))
+Area = area(A)
